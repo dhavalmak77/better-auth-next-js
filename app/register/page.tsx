69288640d98f78e264088b775082b982203d4bb1
Dhaval Makwana <dhavalmak77@gmail.com>
2025-09-30 18:24:48 +0530

    create register form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,49 @@
+'use client';
+
+import { useState } from "react";
+
 const Register = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSubmit = async () => {
+		await {};
+	};
+
 	return (
-		<div className="text-3xl">
-			Register
-		</div>
+		<>
+			<div className="text-4xl text-center">
+				Register
+			</div>
+			<form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-4 mx-auto w-lg">
+				<input
+					type="text"
+					id="name"
+					placeholder="Name"
+					value={name}
+					onChange={e => setName(e.target.value)}
+					className="border px-3 py-2 rounded"
+				/>
+				<input
+					type="email"
+					id="email"
+					placeholder="Email"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					className="border px-3 py-2 rounded"
+				/>
+				<input
+					type="password"
+					id="password"
+					placeholder="Password"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					className="border px-3 py-2 rounded"
+				/>
+				<button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">Register</button>
+			</form>
+		</>
 	);
 }
 
